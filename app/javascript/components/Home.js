3a3f5a533786716ac1f4a37020107fddda3e0144
Vitto De La Paz <vitto@intelimina.com>
2021-05-30 00:30:35 +0800

    Initial category list.

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ExpenseContext } from './App'
+import Card from 'react-bootstrap/Card'
 
-const Home = () => <div>Home</div>
+const Home = () => {
+    const { categories } = useContext(ExpenseContext)
+
+    const categoryRows = categories.map((c) => (
+        <Card>
+            <Card.Body>
+                <Card.Title>{c.title}</Card.Title>
+                <Card.Text>{c.description}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                {c.expenses.length} expense{c.expenses.length > 1 ? 's' : ''}{' '}
+                listed
+            </Card.Footer>
+        </Card>
+    ))
+
+    return (
+        <div className="py-3">
+            <h1 className="mb-4">Categories</h1>
+            <div>{categoryRows}</div>
+        </div>
+    )
+}
 
 export default Home
