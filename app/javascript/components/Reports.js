3a2b45b4467155c4e15e62ffc1d5223b9faf6482
Vitto De La Paz <vitto@intelimina.com>
2021-05-30 16:08:18 +0800

    Initial reports.
    Top 30 expenses in the last 30 days.

--- app/javascript/components/Reports.js
+++ app/javascript/components/Reports.js
@@ -1,5 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ExpenseContext } from './App'
+import Card from 'react-bootstrap/Card'
+import { currencyFormat } from './utils'
 
-const Reports = () => <div>Reports</div>
+const Reports = () => {
+    const { categories } = useContext(ExpenseContext)
+    const categoriesWithAMonthOldExpenses = categories.map((c) => {
+        const today = new Date()
+        const aMonthAgo = new Date(new Date().setDate(today.getDate() - 30))
+        return {
+            ...c,
+            expenses: c.expenses.filter(
+                (e) =>
+                    new Date(e.date) >= aMonthAgo && new Date(e.date) <= today
+            ),
+        }
+    })
+    const categoriesTotalExpenseInAMonth = categoriesWithAMonthOldExpenses.map(
+        (c) => {
+            return {
+                id: c.id,
+                title: c.title,
+                total: c.expenses.reduce(
+                    (acc, curr) => acc + Number(curr.value),
+                    0
+                ),
+            }
+        }
+    )
+    const totalExpenseInAMonth = categoriesTotalExpenseInAMonth.reduce(
+        (acc, curr) => acc + curr.total,
+        0
+    )
+    const categoriesInAMonthReportRows = [...categoriesTotalExpenseInAMonth]
+        .sort((a, b) => b.total - a.total)
+        .map((c) => (
+            <Card body key={c.id} className="mb-3">
+                <Card.Title>{c.title}</Card.Title>
+                <Card.Text>
+                    Php {currencyFormat(c.total)} (
+                    {((c.total / totalExpenseInAMonth) * 100).toFixed(2)}%)
+                </Card.Text>
+            </Card>
+        ))
+
+    return (
+        <div className="py-3">
+            <h1 className="mb-4">Top expenses in the last 30 days</h1>
+            {categoriesInAMonthReportRows}
+        </div>
+    )
+}
 
 export default Reports
